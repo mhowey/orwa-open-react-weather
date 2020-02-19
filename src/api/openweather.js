2285330000dff973abd3c778fb0aa551b530c931
Matt Howey <howey.matt@gmail.com>
2020-02-19 08:24:56 -0500

    moves API key to .env file, adds dotenv package, adds .env to .gitignore

--- src/api/openweather.js
+++ src/api/openweather.js
@@ -2,13 +2,15 @@ import axios from 'axios'
 
 // request format
 // https://api.openweathermap.org/data/2.5/weather?appid={appid}&lon={lon}&lat={lat}
-// appid: '61c2038a004b0641dd136a68d77098c5',
-// local postman mock: https://60d2f55a-271e-4bd7-9de0-3d52897392a3.mock.pstmn.io/
 
+// you'll need to get your own API key from openweathermap.com
+// https://openweathermap.org/api
+// then go to the .env file and put it there... 
 export default axios.create({
-  // baseURL: 'https://60d2f55a-271e-4bd7-9de0-3d52897392a3.mock.pstmn.io/',
   baseURL: 'https://api.openweathermap.org/data/2.5/weather',
   params: {
-    appid: '61c2038a004b0641dd136a68d77098c5',
+    appid: process.env.REACT_APP_ID,
   },
 })
+
+console.log('env::', process.env)
